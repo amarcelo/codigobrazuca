c0611a7501a9820e17d22e14dfe743ae53838753
Tony <antonmarcelo@gmail.com>
2023-04-30 10:02:58 -0300

    Update mytoken.sol

--- solidity/token/mytoken.sol
+++ solidity/token/mytoken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @custom:security-contact antonmarcelo@gmail.com
+contract Brazuca is ERC20 {
+    constructor() ERC20("Brazuca", "BZC") {
+        _mint(msg.sender, 2000 * 10 ** decimals());
+    }
+}
